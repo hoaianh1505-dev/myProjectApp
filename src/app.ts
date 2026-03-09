5091a7758d3ab51d5e22e3cc8cb5723fd0edad77
Do Hoai Anh <anhd78428@gmail.com>
2026-03-09 19:00:04 +0700

    set up .env

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 // const express = require("express");
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
-const port = 8080;
-
+const PORT = process.env.PORT || 8080;
 app.get("/", (req, res) => {
     res.send("Hello World! haha");
 });
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port} with address http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT} with address http://localhost:${PORT}`);
 });
